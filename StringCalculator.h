18c16f55fa4ca76268a0db321fdfbe1c4178a108
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-14 11:39:40 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,3 +1,28 @@
+int stringcount(const char* input)
+{
+  int count;
+  int i=0;
+while(input[i++]!='\0')
+   {
+     count++;
+   }
+  return count;
+}
+
+bool positiveno(int a,const char* input)
+{
+  return (a==1 && (input<=1000) && (input>=0));
+}
+
+char string_nodelimiter(const char* input)
+{
+  int a;
+  a=stringcount(input);
+  if(positiveno(a,input))
+  {
+    return input;
+  }
+}
 
 int add(const char* input)
 {
